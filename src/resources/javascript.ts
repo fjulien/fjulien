36d344e20f458b186e32447bf8235f83851d3ca5
fjulien <fettingerjulien@yahoo.fr>
2023-01-02 12:59:35 +0000

    add ressource folder

---
+++ src/resources/javascript.ts
@@ -0,0 +1,85 @@
+import type { DetailsType } from "../types";
+
+export const javascript: DetailsType = {
+    title: "Javascript (navigateur)",
+    contents: [
+        "Ecmascript",
+        "DOM",
+        "AJAX",
+        "XHR",
+        {
+            title: "Base du language",
+            contents: [
+                "hoisting",
+                "scope",
+                "var / let / const",
+                "Math",
+                "switch",
+                "conditions",
+                "ternaire",
+                "operateur logique",
+                "use strict",
+                {
+                    title: "boucle",
+                    contents: ["for", "for in", "for of", "while", "do while"],
+                },
+                {
+                    title: "variable",
+                    contents: [
+                        {
+                            title: "primitive",
+                            contents: [
+                                "boolean",
+                                "number",
+                                "String",
+                                "undefined",
+                                "Synbol",
+                                "NaN",
+                                "null",
+                                "Circular",
+                            ],
+                        },
+                        {
+                            title: "Complexe",
+                            contents: [
+                                "JSON",
+                                "Map",
+                                "Set",
+                                "Proxi",
+                                {
+                                    title: "Array",
+                                    contents: [
+                                        "foreach",
+                                        "map",
+                                        "filter",
+                                        "reduce",
+                                        "autres",
+                                    ],
+                                },
+                                { title: "Object", contents: [] },
+                            ],
+                        },
+                    ],
+                },
+
+                "Destructuration",
+                "Spread opératuer",
+                "closue",
+                "Curring",
+                {
+                    title: "Asyncrone",
+                    contents: ["callback", "then", "async / await"],
+                },
+                {
+                    title: "Fonction",
+                    contents: [
+                        "Arrow",
+                        "Auto invoked",
+                        "generator",
+                        "r2cursiviter",
+                    ],
+                },
+            ],
+        },
+    ],
+};
